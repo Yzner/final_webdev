689d2444cf211d7fb1518cd498c6109a70c750c6
Yzner <llorenz.10022002@gmail.com>
2024-06-01 00:18:35 +0800

    ksak

--- web/pages/api/signup.js
+++ web/pages/api/signup.js
@@ -1,11 +1,29 @@
 
+// import { query } from '../../utils/db';
+
+// export default async function handler(req, res) {
+//   const { firstname, email, password } = req.body;
+//   const result = await query(
+//     `INSERT INTO users (firstname, email, password) VALUES (?, ?, ?)`,
+//     [firstname, email, password]
+//   );
+//   res.status(201).json(result);
+// }
+
+
+
 import { query } from '../../utils/db';
 
 export default async function handler(req, res) {
-  const { firstname, email, password } = req.body;
-  const result = await query(
-    `INSERT INTO users (firstname, email, password) VALUES (?, ?, ?)`,
-    [firstname, email, password]
-  );
-  res.status(201).json(result);
+  if (req.method === 'POST') {
+    const { firstname, email, password } = req.body;
+    const result = await query(
+      `INSERT INTO users (firstname, email, password) VALUES (?, ?, ?)`,
+      [firstname, email, password]
+    );
+    res.status(201).json(result);
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
