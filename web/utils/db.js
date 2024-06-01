54fe678bedc55b3b8cc0ae1e967e03bad1c418a0
Yzner <llorenz.10022002@gmail.com>
2024-06-01 09:53:15 +0800

    asa

--- web/utils/db.js
+++ web/utils/db.js
@@ -1,29 +1,29 @@
 
-// import mysql from 'mysql2/promise';
-
-// export async function query(sql, values) {
-//   const db = await mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'renz',
-//     database: 'webfin',
-//   });
-//   const [results] = await db.execute(sql, values);
-//   db.end();
-//   return results;
-// }
-
-
 import mysql from 'mysql2/promise';
 
 export async function query(sql, values) {
   const db = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: 'localhost',
+    user: 'root',
+    password: 'renz',
+    database: 'webfin',
   });
   const [results] = await db.execute(sql, values);
   db.end();
   return results;
 }
+
+
+// import mysql from 'mysql2/promise';
+
+// export async function query(sql, values) {
+//   const db = await mysql.createConnection({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//   });
+//   const [results] = await db.execute(sql, values);
+//   db.end();
+//   return results;
+// }
